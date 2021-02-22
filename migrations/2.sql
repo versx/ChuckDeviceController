98934c2c97890df5554366209e96de45ec05f20b
versx <versefx@gmail.com>
2021-02-22 00:45:33 -0800

    Add first_seen_timestamp for spawnpoints

--- migrations/2.sql
+++ migrations/2.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `instance`
 MODIFY COLUMN `type` enum('circle_pokemon','circle_raid','circle_smart_raid','auto_quest','pokemon_iv','bootstrap','find_tth') NOT NULL;
+ALTER TABLE `spawnpoint`
+ADD COLUMN `first_seen_timestamp` int(11) unsigned NOT NULL DEFAULT 0;
