2bda2eca6d7c9f9c7e43cb4c7388537859e5020c
Furtif <msntchat@hotmail.fr>
2021-03-01 00:55:39 +0100

    :S

--- src/Data/Repositories/PokemonRepository.cs
+++ src/Data/Repositories/PokemonRepository.cs
@@ -66,7 +66,7 @@
                 if (ex.Message.Contains("foreign key constraint fails"))
                 {
                     // TODO: noting ??
-                    ConsoleExt.WriteWarn("[PokemonRepository] AddOrUpdateAsync: foreign key constraint fails, or Pokemon not needs update.");
+                    ConsoleExt.WriteInfo("[PokemonRepository] AddOrUpdateAsync: Pokemon not needs update.");
                 }
                 else
                 {
