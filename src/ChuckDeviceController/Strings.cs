f6e5b6f1303c24494264a746b4aa57ac1950651e
versx <versx@ver.sx>
2021-03-06 18:54:50 -0800

    Move wwwroot

--- src/ChuckDeviceController/Strings.cs
+++ src/ChuckDeviceController/Strings.cs
@@ -9,7 +9,7 @@
         public const string ViewsFolder = "Views";
         public const string TemplateExt = ".mustache";
 
-        public const string WebRoot = "../../wwwroot";
+        public const string WebRoot = "wwwroot";
         public const string DataFolder = WebRoot + "/static/data";
         public const string MigrationsFolder = "../../migrations";
 
