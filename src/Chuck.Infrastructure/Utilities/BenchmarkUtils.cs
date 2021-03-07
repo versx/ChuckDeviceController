26af7c7d38259891a416773916966f7865700666
versx <versx@ver.sx>
2021-03-07 12:35:05 -0800

    Fix gym/raid/egg webhooks

---
+++ src/Chuck.Infrastructure/Utilities/BenchmarkUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace Chuck.Infrastructure.Utilities
+{
+    using System;
+    using System.Diagnostics;
+
+    using Chuck.Infrastructure.Extensions;
+
+    public static class BenchmarkUtils
+    {
+        public static void BenchmarkMethod(Func<int> method, string type)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var count = method();
+            stopwatch.Stop();
+            if (count > 0)
+            {
+                ConsoleExt.WriteInfo($"[DataConsumer] {type} Count: {count:N0} parsed in {stopwatch.Elapsed.TotalSeconds}s");
+            }
+        }
+    }
+}
