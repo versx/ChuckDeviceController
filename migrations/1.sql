695439f5d3bd3c0352b26429b4c9360534bb631b
versx <versx@ver.sx>
2021-03-08 12:07:01 -0800

    Fix migration typo

--- migrations/1.sql
+++ migrations/1.sql
@@ -44,6 +44,7 @@ CREATE TABLE `webhook` (
    `delay` double DEFAULT 5,
    `types` longtext DEFAULT NULL,
    `geofence` varchar(255) DEFAULT NULL,
+   `data` longtext NOT NULL,
    `enabled` tinyint(1) unsigned DEFAULT 1,
    PRIMARY KEY `name` (`name`),
    CONSTRAINT `fk_webhook_geofence_name` FOREIGN KEY (`geofence`) REFERENCES `geofence` (`name`) ON DELETE SET NULL ON UPDATE CASCADE
