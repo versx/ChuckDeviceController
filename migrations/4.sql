ac9eea1faa69f7c14a3904f4ebfc800b3a6c6f85
versx <versx@ver.sx>
2021-03-10 13:14:49 -0800

    Update old geofence values to list

--- migrations/4.sql
+++ migrations/4.sql
@@ -3,3 +3,6 @@ DROP FOREIGN KEY `fk_geofence_name`;
 
 ALTER TABLE `instance` 
 CHANGE `geofence` `geofences` longtext NOT NULL;
+
+UPDATE `instance` 
+SET geofences = CONCAT("[\"", geofences, "\"]");
