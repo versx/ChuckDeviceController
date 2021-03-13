eb295c43d96cf5e94c2c3432600062f75fa7ba32
versx <versx@ver.sx>
2021-03-12 17:05:59 -0800

    Update 6.sql

--- migrations/6.sql
+++ migrations/6.sql
@@ -2,7 +2,7 @@ ALTER TABLE `webhook`
 DROP FOREIGN KEY `fk_webhook_geofence_name`;
 
 ALTER TABLE `webhook` 
-CHANGE `geofence` `geofences` longtext DEFAULT NULL;
+CHANGE COLUMN `geofence` `geofences` longtext DEFAULT NULL;
 
 UPDATE `webhook` 
 SET geofences = CONCAT("[\"", geofences, "\"]");
