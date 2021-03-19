e44359478d6264f53abd7b49705d532668fce1e3
versx <versx@ver.sx>
2021-03-18 23:09:18 -0700

    Remove dead code

--- src/Chuck.Geometry/Models/MultiPolygon.cs
+++ src/Chuck.Geometry/Models/MultiPolygon.cs
@@ -42,43 +42,6 @@
             return result;
         }
 
-        public List<S2CellId> GetS2CellIDs(ushort minLevel, ushort maxLevel, int maxCells)
-        {
-            var bbox = GetBoundingBox();
-            var regionCoverer = new S2RegionCoverer
-            {
-                MinLevel = minLevel,
-                MaxLevel = maxLevel,
-                MaxCells = maxCells,
-            };
-            var region = new S2LatLngRect(
-                S2LatLng.FromDegrees(bbox.MinimumLatitude, bbox.MinimumLongitude),
-                S2LatLng.FromDegrees(bbox.MaximumLatitude, bbox.MaximumLongitude)
-            );
-            var cellIDsBBox = regionCoverer.GetInteriorCovering(region);
-            var cellIDs = new List<S2CellId>();
-            foreach (var cellId in cellIDsBBox)
-            {
-                var cell = new S2Cell(cellId);
-                var vertex0 = cell.GetVertex(0); //south
-                var vertex1 = cell.GetVertex(1); //east
-                var vertex2 = cell.GetVertex(2); //north
-                var vertex3 = cell.GetVertex(3); //west
-                var coord0 = new S2LatLng(new S2Point(vertex0.X, vertex0.Y, vertex0.Z));
-                var coord1 = new S2LatLng(new S2Point(vertex1.X, vertex1.Y, vertex1.Z));
-                var coord2 = new S2LatLng(new S2Point(vertex2.X, vertex2.Y, vertex2.Z));
-                var coord3 = new S2LatLng(new S2Point(vertex3.X, vertex3.Y, vertex3.Z));
-                if (GeofenceService.InPolygon(this, coord0.LatDegrees, coord0.LngDegrees) ||
-                    GeofenceService.InPolygon(this, coord1.LatDegrees, coord1.LngDegrees) ||
-                    GeofenceService.InPolygon(this, coord2.LatDegrees, coord2.LngDegrees) ||
-                    GeofenceService.InPolygon(this, coord3.LatDegrees, coord3.LngDegrees))
-                {
-                    cellIDs.Add(cellId);
-                }
-            }
-            return cellIDs;
-        }
-
         public BoundingBox GetBoundingBox()
         {
             // Add checks here, if necessary, to make sure that points is not null,
