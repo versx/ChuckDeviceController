1b0eb7b70200d5efcb58799a99d065a21e71f31a
versx <versx@ver.sx>
2022-07-09 01:37:06 -0700

    Edit and delete users, prevent root account from being deleted by dummies.

--- src/ChuckDeviceConfigurator/Controllers/UserController.cs
+++ src/ChuckDeviceConfigurator/Controllers/UserController.cs
@@ -48,13 +48,17 @@
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
-                ViewBag.ErrorMessage = $"User with Id '{userId}' not found";
+                ViewBag.ErrorMessage = $"User account with Id '{userId}' does not exist";
                 return View("NotFound");
             }
 
             ViewBag.userId = userId;
             ViewBag.UserName = user.UserName;
-            var model = new List<ManageUserRolesViewModel>();
+            var model = new ManageUserViewModel
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+            };
             foreach (var role in _roleManager.Roles)
             {
                 var userRolesViewModel = new ManageUserRolesViewModel
@@ -63,36 +67,93 @@
                     RoleName = role.Name,
                     Selected = false,// TODO: Fix "This MySqlConnection is already in use" await _userManager.IsInRoleAsync(user, role.Name),
                 };
-                model.Add(userRolesViewModel);
+                model.Roles.Add(userRolesViewModel);
             }
             return View(model);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Manage(List<ManageUserRolesViewModel> model, string userId)
+        public async Task<IActionResult> Manage(ManageUserViewModel model, string userId)
         {
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
-                return View();
-            }
-            var roles = await _userManager.GetRolesAsync(user);
-            var result = await _userManager.RemoveFromRolesAsync(user, roles);
-            if (!result.Succeeded)
-            {
-                ModelState.AddModelError("", "Cannot remove user existing roles");
+                ModelState.AddModelError("", $"User account with id '{userId}' does not exist");
                 return View(model);
             }
 
-            result = await _userManager.AddToRolesAsync(user, model.Where(x => x.Selected).Select(y => y.RoleName));
-            if (!result.Succeeded)
+            // Check if any existing user accounts are registered with new email that are not the existing user
+            if (_userManager.Users.FirstOrDefault(user => user.Email == model.Email && user.Id != userId) != null)
             {
-                ModelState.AddModelError("", "Cannot add selected roles to user");
+                ModelState.AddModelError("", $"User account with email '{model.Email}' already exists, please use a different email address.");
                 return View(model);
             }
 
-            return RedirectToAction("Index");
+            // Only update user account password if 'Password' and 'ConfirmPassword' fields are set
+            if (!string.IsNullOrEmpty(model.Password) && !string.IsNullOrEmpty(model.ConfirmPassword))
+            {
+                if (model.Password != model.ConfirmPassword)
+                {
+                    // Passwords do not match
+                    ModelState.AddModelError("", $"Provided password and confirm password do not match.");
+                    return View(model);
+                }
+                else
+                {
+                    // Passwords match, proceed with updating user account password by removing the exiting password used
+                    await _userManager.RemovePasswordAsync(user);
+                    // Add new password to user account
+                    await _userManager.AddPasswordAsync(user, model.Password);
+                }
+            }
+
+            // Assign new email address only if it does not match existing email address for user account
+            if (user.Email != model.Email)
+            {
+                // TODO: Add some type of verification that the user owns the email account
+                // Assign user new email address
+                user.Email = model.Email;
+
+                // Update user account email address
+                var result = await _userManager.UpdateAsync(user);
+                if (!result.Succeeded)
+                {
+                    var errors = string.Join("\n", result.Errors.Select(err => err.Description));
+                    ModelState.AddModelError("", errors);
+                    return View(model);
+                }
+            }
+
+            // Check if any roles selected before updating user account's assigned roles
+            if (model.Roles.Any(role => role.Selected))
+            {
+                // Get select role names
+                var selectedRoles = model.Roles.Where(role => role.Selected)
+                                               .Select(role => role.RoleName)
+                                               .ToList();
+
+                // Fetch all existing roles assigned to user account
+                var roles = await _userManager.GetRolesAsync(user);
+
+                // Remove all assigned roles from user
+                var removeResult = await _userManager.RemoveFromRolesAsync(user, roles);
+                if (!removeResult.Succeeded)
+                {
+                    ModelState.AddModelError("", $"Cannot remove already assigned roles from user account '{model.UserName}'");
+                    return View(model);
+                }
+
+                // Assign user account new list of expected roles
+                var addResult = await _userManager.AddToRolesAsync(user, selectedRoles);
+                if (!addResult.Succeeded)
+                {
+                    ModelState.AddModelError("", $"Cannot assign selected roles to user account '{model.UserName}'");
+                    return View(model);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Create()
@@ -121,12 +182,12 @@
         {
             try
             {
-                if (_userManager.Users.FirstOrDefault(user => user.UserName == model.UserName) != null)
+                if (_userManager.FindByNameAsync(model.UserName) != null)
                 {
                     ModelState.AddModelError("User", $"User account by name '{model.UserName}' already exists, please choose a different username. It is also possible to use your email address as your username.");
                     return View(model);
                 }
-                if (_userManager.Users.FirstOrDefault(user => user.Email == model.Email) != null)
+                if (_userManager.FindByEmailAsync(model.Email) != null)
                 {
                     ModelState.AddModelError("User", $"User account with email '{model.Email}' already exists, please use a different email address.");
                     return View(model);
@@ -189,6 +250,48 @@
             {
                 ModelState.AddModelError("User", $"Unknown error occurred while creating new user account.");
                 return View(model);
+            }
+        }
+
+        public async Task<IActionResult> Delete(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                // Failed to retrieve user account from database, does it exist?
+                ModelState.AddModelError("User", $"User account does not exist with username '{userId}'.");
+                return View();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(ApplicationUser user, string userId)
+        {
+            try
+            {
+                var userAccount = await _userManager.FindByIdAsync(userId);
+                if (userAccount == null)
+                {
+                    // Failed to retrieve user account from database, does it exist?
+                    ModelState.AddModelError("User", $"User account does not exist with username '{userId}'.");
+                    return View(user);
+                }
+
+                // Delete user account from database
+                var result = await _userManager.DeleteAsync(userAccount);
+                if (!result.Succeeded)
+                {
+                    return View(userAccount);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("User", $"Unknown error occurred while deleting user account '{userId}'.");
+                return View(user);
             }
         }
 
