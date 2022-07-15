e75db19e9a13e961a24d7648bd904e58e1211846
versx <versx@ver.sx>
2022-07-15 01:10:27 -0700

    Add detailed comments for Geometry.Models.BoundingBox class

--- src/Libs/ChuckDeviceController.Geometry/Models/BoundingBox.cs
+++ src/Libs/ChuckDeviceController.Geometry/Models/BoundingBox.cs
@@ -1,20 +1,52 @@
 ﻿namespace ChuckDeviceController.Geometry.Models
 {
+    /// <summary>
+    /// Square or rectangular bounding box boundary for an area/geofence.
+    /// </summary>
     public class BoundingBox
     {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the minimum latitude of the bounding box.
+        /// </summary>
         public double MinimumLatitude { get; set; } // minX
 
+        /// <summary>
+        /// Gets or sets the maximum latitude of the bounding box.
+        /// </summary>
         public double MaximumLatitude { get; set; } // maxX
 
+        /// <summary>
+        /// Gets or sets the minimum longitude of the bounding box.
+        /// </summary>
         public double MinimumLongitude { get; set; } // minY
 
+        /// <summary>
+        /// Gets or sets the maximum longitude of the bounding box.
+        /// </summary>
         public double MaximumLongitude { get; set; } // maxY
 
-        public bool IsInBoundingBox(double lat, double lon)
+        #endregion
+
+        /// <summary>
+        ///     Check if the specified latitude and longitude are within the
+        ///     bounding box boundaries.
+        /// </summary>
+        /// <param name="latitude">
+        ///     The latitude of the geocoordinate.
+        /// </param>
+        /// <param name="longitude">
+        ///     The longitude of the geocoordinate.
+        /// </param>
+        /// <returns>
+        ///     Returns <c>true</c> if the coordinate is within the bounding boxes boundaries.
+        /// </returns>
+        public bool IsInBoundingBox(double latitude, double longitude)
         {
-            var result = 
-                lat >= MinimumLatitude && lon >= MinimumLongitude &&
-                lat <= MaximumLatitude && lon <= MaximumLongitude;
+            var result =
+                latitude >= MinimumLatitude && longitude >= MinimumLongitude &&
+                latitude <= MaximumLatitude && longitude <= MaximumLongitude;
             return result;
         }
     }
