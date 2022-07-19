9200dec74d23c5633da1bcfc55032543703b8fe0
versx <versx@ver.sx>
2022-07-18 22:46:23 -0700

    Add accurate circle route time calculation and quick refactor of circle job controller instance class

--- src/ChuckDeviceConfigurator/JobControllers/CircleInstanceController.cs
+++ src/ChuckDeviceConfigurator/JobControllers/CircleInstanceController.cs
@@ -16,7 +16,7 @@
         private static readonly Random _random = new();
         private readonly ILogger<CircleInstanceController> _logger;
         private readonly Dictionary<string, DeviceIndex> _currentUuid = new();
-        private uint _lastIndex = 0; // Used for basic leap frog routing
+        private int _lastIndex = 0; // Used for basic leap frog routing
         private double _lastCompletedTime;
         private double _lastLastCompletedTime;
 
@@ -94,6 +94,7 @@
                 case CircleInstanceType.Raid:
                     switch (RouteType)
                     {
+                        // TODO: Eventually remove leap frog routing logic (cough, remove all circle routing instances all together)
                         case CircleInstanceRouteType.Default:
                             // Get default leap frog route
                             currentCoord = BasicRoute();
@@ -126,13 +127,45 @@
         public virtual async Task<string> GetStatusAsync()
         {
             var status = Strings.DefaultInstanceStatus;
-            if (_lastCompletedTime > 0 && _lastLastCompletedTime > 0)
+            if (RouteType == CircleInstanceRouteType.Default)
             {
-                var timeDiffSeconds = _lastCompletedTime - _lastLastCompletedTime;
-                if (timeDiffSeconds > 0)
+                // Gets the basic leap frog routing logic round time of any (the last) completed
+                // device assigned to the route that has completed it.
+                if (_lastCompletedTime > 0 && _lastLastCompletedTime > 0)
                 {
-                    var time = Math.Round(timeDiffSeconds, 2);
+                    var timeDiffSeconds = _lastCompletedTime - _lastLastCompletedTime;
+                    if (timeDiffSeconds > 0)
+                    {
+                        var time = Math.Round(timeDiffSeconds, 2);
+                        status = $"Round Time: {time:N0}s";
+                    }
+                }
+            }
+            else
+            {
+                // TODO: Get total average of all devices last completed time
+                // Get the sum of all route round times for all devices if they have completed the
+                // route at least once. (maybe average too?)
+                var totalRoundTime = _currentUuid.Values.Sum(device =>
+                    // If device hasn't completed the route yet, or rather its assigned coordinates
+                    // list, use 0 for route last completed sum calculation.
+                    device.LastCompleted == 0 && device.LastCompletedWholeRoute == 0
+                    ? 0
+                    : (double)device.LastCompleted - (double)device.LastCompletedWholeRoute
+                );
+                if (totalRoundTime > 0)
+                {
+                    // Only show average route round time if at least one device has completed it.
+                    var time = Math.Round(totalRoundTime);
+                    // TODO: Format round trip time status by hours/minutes/seconds instead of just seconds.
                     status = $"Round Time: {time:N0}s";
+                }
+                else
+                {
+                    // No assigned devices have completed the route yet, show their current route indexes out
+                    // of the total amount of coordinates for the route until one has fully completed it.
+                    var indexesStatus = string.Join(", ", _currentUuid.Values.Select(uuid => uuid.LastRouteIndex));
+                    status = $"Route Indexes: {indexesStatus}/{Coordinates.Count}";
                 }
             }
             return await Task.FromResult(status);
@@ -159,12 +192,12 @@
 
         private Coordinate BasicRoute()
         {
-            var currentIndex = (int)_lastIndex;
+            var currentIndex = _lastIndex;
             var currentCoord = Coordinates[currentIndex];
             // Check if current index is last in coordinates list,
             // if so we've completed the route. Reset route to first
             // coordinate for next device.
-            if (_lastIndex + 1 == Coordinates.Count)
+            if (_lastIndex == Coordinates.Count)
             {
                 _lastLastCompletedTime = _lastCompletedTime;
                 _lastCompletedTime = DateTime.UtcNow.ToTotalSeconds();
@@ -181,10 +214,11 @@
         {
             // TODO: Lock index
             var currentUuidIndex = _currentUuid.ContainsKey(uuid)
-                ? _currentUuid[uuid].Index
+                ? _currentUuid[uuid].LastRouteIndex
                 : Convert.ToInt32(Math.Round(Convert.ToDouble(_random.Next(ushort.MinValue, ushort.MaxValue) % Coordinates.Count)));
 
             var shouldAdvance = true;
+            // TODO: Random from 0 to Coordinates.Count
             var offsetValue = _random.Next(ushort.MinValue, ushort.MaxValue) % 100;
             if (offsetValue < 5)
             {
@@ -213,9 +247,9 @@
                     if (currentUuidIndex >= Coordinates.Count)
                     {
                         currentUuidIndex = 0;
-                        // This is an approximation of round time.
-                        _lastLastCompletedTime = _lastCompletedTime;
-                        _lastCompletedTime = now;
+                        // This is an approximation of round time per device
+                        _currentUuid[uuid].LastCompletedWholeRoute = _currentUuid[uuid].LastCompleted;
+                        _currentUuid[uuid].LastCompleted = now;
                     }
                 }
                 else
@@ -228,11 +262,10 @@
                     }
                 }
             }
-            _currentUuid[uuid] = new DeviceIndex
-            {
-                Index = currentUuidIndex,
-                LastSeen = now,
-            };
+
+            _currentUuid[uuid].LastRouteIndex = currentUuidIndex;
+            _currentUuid[uuid].LastSeen = now;
+
             var currentCoord = Coordinates[currentUuidIndex];
             return currentCoord;
         }
@@ -240,21 +273,27 @@
         internal Coordinate SmartRoute(string uuid)
         {
             var now = DateTime.UtcNow.ToTotalSeconds();
-            var currentUuidIndex = _currentUuid.ContainsKey(uuid)
-                ? _currentUuid[uuid].Index
+            // TODO: ContainsKey check is probably redundant since we add the device to the current
+            // device list in the GetTaskAsync method. Need to confirm but 99.99% possitive the random
+            // index is never used.
+
+            // Check if current device cache contains device and that the last route index is greater
+            // than 0 as well as more than one device is assigned to the instance. Otherwise give the
+            // device a random route index to start from.
+            var currentUuidIndex = _currentUuid.ContainsKey(uuid) && _currentUuid.Count > 0
+                ? _currentUuid[uuid].LastRouteIndex
                 : _random.Next(0, Coordinates.Count);
 
-            _currentUuid[uuid] = new DeviceIndex
-            {
-                Index = currentUuidIndex,
-                LastSeen = now,
-            };
+            _currentUuid[uuid].LastRouteIndex = currentUuidIndex;
+            _currentUuid[uuid].LastSeen = now;
 
             var shouldAdvance = true;
             var jumpDistance = 0d;
 
             if (_currentUuid.Count > 1 && _random.Next(0, 100) < 15)
             {
+                // This looks like some wizardry which could possibly be optimized,
+                // and potentially calculated better. :thinking:
                 (uint numLiveDevices, double distanceToNextDevice) = GetDeviceSpacing(uuid);
                 var dist = 10 * distanceToNextDevice * numLiveDevices + 5;
                 if (dist < 10 * Coordinates.Count)
@@ -266,10 +305,13 @@
                     jumpDistance = distanceToNextDevice - Coordinates.Count / numLiveDevices - 1;
                 }
             }
+
+            // Advance device if route index is 0 (beginning) and that there are more than one coordinate specified
             if (currentUuidIndex == 0 && Coordinates.Count > 1)
             {
                 shouldAdvance = true;
             }
+
             if (shouldAdvance)
             {
                 currentUuidIndex += Convert.ToInt32(jumpDistance + 1);
@@ -277,19 +319,24 @@
                 {
                     // Completed route, reset index
                     currentUuidIndex -= Coordinates.Count;
-                    _lastLastCompletedTime = _lastCompletedTime;
-                    _lastCompletedTime = now;
+                    _currentUuid[uuid].LastCompletedWholeRoute = _currentUuid[uuid].LastCompleted;
+                    _currentUuid[uuid].LastCompleted = now;
                 }
             }
             else
             {
+                // Device should not advance forward int he route, check if current device index
+                // is less than 0 after backing up incase we are at the first coordinate. If device's
+                // current index is less than 0 set route index to last coord in list.
                 currentUuidIndex--;
                 if (currentUuidIndex < 0)
                 {
                     currentUuidIndex = Coordinates.Count;
                 }
             }
-            _currentUuid[uuid].Index = currentUuidIndex;
+
+            // Last current device's last route index taken
+            _currentUuid[uuid].LastRouteIndex = currentUuidIndex;
             var currentCoord = Coordinates[currentUuidIndex];
             return currentCoord;
         }
@@ -323,53 +370,33 @@
 
         private (uint, double) GetDeviceSpacing(string uuid)
         {
-            var now = DateTime.UtcNow.ToTotalSeconds();
-            var deadDeviceCutOffTime = now - 60;
-            var liveDevices = new List<string>();
+            var liveDevices = GetLiveDevices();
+            var numLiveDevices = liveDevices.Count;
+            var distanceToNextDevice = (double)Coordinates.Count;
 
-            // Sort indexes by current device index
-            var uuidIndexes = _currentUuid.ToList();
-            uuidIndexes.Sort((pair1, pair2) => pair1.Value.Index.CompareTo(pair2.Value.Index));
-            var dict = uuidIndexes.ToDictionary(key => key.Key, value => value.Value);
-
-            foreach (var (currentUuid, index) in dict)
+            // Check if device is in live device list, possibly a redunant check, need to confirm.
+            if (!liveDevices.Contains(uuid))
             {
-                if (index.LastSeen > 0)
-                {
-                    if (index.LastSeen < deadDeviceCutOffTime)
-                    {
-                        // Device has not updated in the last 60 seconds, assume dead/offline
-                        _currentUuid[currentUuid] = null;
-                    }
-                    else
-                    {
-                        liveDevices.Add(currentUuid);
-                    }
-                }
+                _logger.LogWarning($"[{Name}] [{uuid}] Device is not in live device list, unable to calculate device spacing for job controller instance.");
+                return (0, 0);
             }
 
-            var numLiveDevices = (uint)liveDevices.Count;
-            double distanceToNextDevice = Coordinates.Count;
             for (var i = 0; i < numLiveDevices; i++)
             {
+                // Skip device if not in live device list
                 if (uuid != liveDevices[i])
                     continue;
 
-                var currentUuid = _currentUuid[uuid];
-                if (i < numLiveDevices - 1)
-                {
-                    var nextDevice = liveDevices[i + 1];
-                    var nextUuid = _currentUuid[nextDevice];
-                    distanceToNextDevice = GetRouteDistance(currentUuid.Index, nextUuid.Index);
-                }
-                else
-                {
-                    var nextDevice = liveDevices[i]; // TODO: Check, was `0`
-                    var nextUuid = _currentUuid[nextDevice];
-                    distanceToNextDevice = GetRouteDistance(currentUuid.Index, nextUuid.Index);
-                }
+                var nextDeviceIndex = i < numLiveDevices - 1
+                    ? i + 1
+                    : i; // TODO: Check, was '0'
+                var nextDeviceUuid = liveDevices[nextDeviceIndex];
+                var currentUuidIndex = _currentUuid[uuid].LastRouteIndex;
+                var nextUuidIndex = _currentUuid[nextDeviceUuid].LastRouteIndex;
+                distanceToNextDevice = GetRouteDistance(currentUuidIndex, nextUuidIndex);
             }
-            return (numLiveDevices, distanceToNextDevice);
+
+            return ((uint)numLiveDevices, distanceToNextDevice);
         }
 
         internal void AddDevice(string uuid)
@@ -380,8 +407,46 @@
 
             _currentUuid.Add(uuid, new DeviceIndex
             {
+                // TODO: Set/generate random device index upon adding to device list?
                 LastSeen = DateTime.UtcNow.ToTotalSeconds(),
             });
+        }
+
+        private List<string> GetLiveDevices()
+        {
+            var now = DateTime.UtcNow.ToTotalSeconds();
+            var deadDeviceCutOffTime = now - 60;
+            var liveDevices = new List<string>();
+
+            // Sort indexes by current device index
+            var deviceIndexes = GetDevicesSortedByIndex();
+            foreach (var (currentUuid, index) in deviceIndexes)
+            {
+                // Check if device has requested a job, otherwise skip it
+                if (index.LastSeen == 0)
+                    continue;
+
+                if (index.LastSeen < deadDeviceCutOffTime)
+                {
+                    // Device has not updated in the last 60 seconds, assume dead/offline
+                    _currentUuid[currentUuid] = null;
+                }
+                else
+                {
+                    // Device is active and alive, add to live devices list
+                    liveDevices.Add(currentUuid);
+                }
+            }
+            return liveDevices;
+        }
+
+        private Dictionary<string, DeviceIndex> GetDevicesSortedByIndex()
+        {
+            var devices = _currentUuid.ToList();
+            devices.Sort((pair1, pair2) => pair1.Value.LastRouteIndex.CompareTo(pair2.Value.LastRouteIndex));
+
+            var indexes = devices.ToDictionary(key => key.Key, value => value.Value);
+            return indexes;
         }
 
         #endregion
@@ -389,8 +454,14 @@
 
     internal class DeviceIndex
     {
-        public int Index { get; set; }
+        public int LastRouteIndex { get; set; }
 
         public ulong LastSeen { get; set; }
+
+        public ulong LastCompleted { get; set; }
+
+        // TODO: Actually implement this by checking if device has visited all coordinates,
+        // keep track of coordinates visited I suppose?
+        public ulong LastCompletedWholeRoute { get; set; }
     }
 }
