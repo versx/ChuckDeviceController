52f369b75e6295f099945b20e99e2e3bcbfcb958
versx <versx@ver.sx>
2022-08-31 16:50:26 -0700

    start db triggers

---
+++ src/Libs/ChuckDeviceController.Data/Triggers/PokemonInsertedTrigger.cs
@@ -0,0 +1,34 @@
+﻿namespace ChuckDeviceController.Data.Triggers
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using EntityFrameworkCore.Triggered;
+
+    using ChuckDeviceController.Data.Contexts;
+    using ChuckDeviceController.Data.Entities;
+
+    public class PokemonInsertedTrigger : IAfterSaveTrigger<Pokemon>
+    {
+        private readonly MapContext _context;
+
+        public PokemonInsertedTrigger(MapContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AfterSave(ITriggerContext<Pokemon> context, CancellationToken cancellationToken)
+        {
+            // INSERT new pokemon
+            // ON DUP increment count
+            // If IV NOT NULL
+            // INSERT INTO iv_stats
+            // ON DUP increment count
+            // If IV=100
+            // INSERT INTO hundo_stats
+            // ON DUP increment count
+
+            await Task.CompletedTask;
+        }
+    }
+}
