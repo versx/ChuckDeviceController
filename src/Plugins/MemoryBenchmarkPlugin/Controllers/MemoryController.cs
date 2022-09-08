148aec01cb85f66b1888db6ada18176bdbff3a62
versx <versx@ver.sx>
2022-09-07 19:23:44 -0700

    add MemoryBenchmarkPlugin plugin

---
+++ src/Plugins/MemoryBenchmarkPlugin/Controllers/MemoryController.cs
@@ -0,0 +1,19 @@
+﻿namespace MemoryBenchmarkPlugin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class MemoryController : Controller
+    {
+        private readonly ILogger<MemoryController> _logger;
+
+        public MemoryController(ILogger<MemoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
