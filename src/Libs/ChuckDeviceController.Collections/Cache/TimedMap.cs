c1b09b9dfa826a40a6e294ac52e73755feb18253
versx <versx@ver.sx>
2022-09-08 23:05:02 -0700

    lock TimedMap internal collection

--- src/Libs/ChuckDeviceController.Collections/Cache/TimedMap.cs
+++ src/Libs/ChuckDeviceController.Collections/Cache/TimedMap.cs
@@ -3,6 +3,7 @@
     // Credits: https://codereview.stackexchange.com/questions/229094/leetcode-time-based-key-value-store-c
     public class TimedMap<T>
     {
+        private static readonly object _lock = new();
         private readonly Dictionary<string, List<KeyValuePair<ulong, T>>> _entries;
 
         public TimedMap()
@@ -27,37 +28,43 @@
             }
             list.Add(new(timestamp, value));
             */
-            if (!_entries.ContainsKey(key))
+            lock (_lock)
             {
-                _entries.Add(key, new List<KeyValuePair<ulong, T>>());
+                if (!_entries.ContainsKey(key))
+                {
+                    _entries.Add(key, new List<KeyValuePair<ulong, T>>());
+                }
+                _entries[key].Add(new(timestamp, value));
             }
-            _entries[key].Add(new(timestamp, value));
         }
 
         public T? Get(string key, ulong timestamp)
         {
-            if (!_entries.ContainsKey(key))
+            lock (_lock)
             {
-                return default;
-            }
+                if (!_entries.ContainsKey(key))
+                {
+                    return default;
+                }
 
-            var list = _entries[key];
-            //var i = list.BinarySearch(new KeyValuePair<ulong, T>(timestamp, "}"), new TimeComparer());
-            var i = list.BinarySearch(new KeyValuePair<ulong, T>(timestamp, default), new TimeComparer());
-            if (i >= 0)
-            {
-                return list[i].Value;
-            }
-            else if (i == -1)
-            {
-                return default;
-            }
+                var list = _entries[key];
+                //var i = list.BinarySearch(new KeyValuePair<ulong, T>(timestamp, "}"), new TimeComparer());
+                var i = list.BinarySearch(new KeyValuePair<ulong, T>(timestamp, default), new TimeComparer());
+                if (i >= 0)
+                {
+                    return list[i].Value;
+                }
+                else if (i == -1)
+                {
+                    return default;
+                }
 
-            // If BinarySearch returns a negative number, it respresents the binary complement of
-            // the index of the first item in list that is higher than the item we looked for.
-            // Since we want the first item that is one lower, -1.
-            var tempKey = ~i - 1;
-            return list[tempKey].Value;
+                // If BinarySearch returns a negative number, it respresents the binary complement of
+                // the index of the first item in list that is higher than the item we looked for.
+                // Since we want the first item that is one lower, -1.
+                var tempKey = ~i - 1;
+                return list[tempKey].Value;
+            }
         }
 
         private class TimeComparer : IComparer<KeyValuePair<ulong, T>>
