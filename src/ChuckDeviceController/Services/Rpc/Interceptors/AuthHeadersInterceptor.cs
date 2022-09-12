bd1bd2b1b6b43e05266d8269afc65faaf14a0ca9
versx <versx@ver.sx>
2022-09-11 18:21:16 -0700

    fix jwt cache race condition and clean expired tokens

--- src/ChuckDeviceController/Services/Rpc/Interceptors/AuthHeadersInterceptor.cs
+++ src/ChuckDeviceController/Services/Rpc/Interceptors/AuthHeadersInterceptor.cs
@@ -9,13 +9,24 @@
 
     public class AuthHeadersInterceptor : Interceptor
     {
+        #region Constants
+
         private const string AuthorizationHeader = "Authorization";
         private const string IgnoreJwtValidationHeader = "IgnoreJwtValidation";
         private const string DefaultGrpcServiceIdentifier = "Grpc";
         private const string DefaultInternalServiceIdentifier = "InternalService";
 
-        protected readonly Dictionary<string, ulong> _jwtTokens = new();
+        #endregion
+
+        #region Variables
+
+        protected static readonly Dictionary<string, ulong> _jwtTokens = new();
+        private static readonly object _lock = new();
         private readonly string _grpcConfiguratorServerEndpoint;
+
+        #endregion
+
+        #region Constructor
 
         public AuthHeadersInterceptor(IConfiguration configuration)
         {
@@ -26,6 +37,10 @@
             }
             _grpcConfiguratorServerEndpoint = configuratorEndpoint;
         }
+
+        #endregion
+
+        #region Impl Override Methods
 
         public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
             TRequest request,
@@ -46,32 +61,32 @@
             return base.AsyncUnaryCall(request, context, continuation);
         }
 
-        #region Private Methods
+        #endregion
+
+        #region Token Methods
 
         private async Task<Metadata?> GetAuthorizationTokenAsync()
         {
-            if (_jwtTokens.Any())
+            // Check if any JWT tokens have been cached
+            lock (_lock)
             {
-                var token = GetCachedAccessToken();
-                if (!string.IsNullOrEmpty(token))
+                if (_jwtTokens.Any())
                 {
-                    return CreateMetadata(token);
+                    // Retrieve valid JWT access token from cache, otherwise generate new one
+                    var token = GetCachedAccessToken();
+                    // Ensure JWT token is not null
+                    if (!string.IsNullOrEmpty(token))
+                    {
+                        // Create gRPC request headers with JWT token set
+                        // to 'Authorization' header.
+                        return CreateMetadata(token);
+                    }
                 }
             }
 
-            using var channel = GrpcChannel.ForAddress(_grpcConfiguratorServerEndpoint);
-
-            var client = new JwtAuth.JwtAuthClient(channel);
-            var request = new JwtAuthRequest
-            {
-                Identifier = DefaultGrpcServiceIdentifier,
-            };
-
-            var headers = new Metadata
-            {
-                { IgnoreJwtValidationHeader, "1" },
-            };
-            var response = await client.GenerateAsync(request, headers);
+            // Send generate JWT token request
+            var response = await SendGenerateJwtTokenRequest();
+            // Ensure we received a response and that it was successful
             if (response == null || response.Status != JwtAuthStatus.Ok)
             {
                 return null;
@@ -80,38 +95,98 @@
             // Cache access token
             CacheAccessToken(response);
 
-            // Cleanly shutdown gRPC client channel
-            await channel.ShutdownAsync();
-
+            // Create gRPC request headers with JWT token set
+            // to 'Authorization' header.
             return CreateMetadata(response.AccessToken);
         }
 
-        private string GetCachedAccessToken()
+        private async Task<JwtAuthResponse> SendGenerateJwtTokenRequest()
         {
-            // Get first token, check if expired - if so generate new one otherwise use existing
+            using var channel = GrpcChannel.ForAddress(_grpcConfiguratorServerEndpoint);
+            var client = new JwtAuth.JwtAuthClient(channel);
+            var request = new JwtAuthRequest
+            {
+                Identifier = DefaultGrpcServiceIdentifier,
+            };
+
+            var headers = CreateJwtMetadata();
+            var response = await client.GenerateAsync(request, headers);
+
+            // Cleanly shutdown gRPC client channel
+            await channel.ShutdownAsync();
+
+            return response;
+        }
+
+        #endregion
+
+        #region Cache Methods
+
+        private static string GetCachedAccessToken()
+        {
             var now = DateTime.UtcNow.ToTotalSeconds();
             // Attempt to retrieve the first JWT token that hasn't expired
-            var (token, expires) = _jwtTokens.First(x => x.Value - now > 0 && x.Value > 0);
+            var (token, expires) = _jwtTokens.FirstOrDefault(x => x.Value - now > 0 && x.Value > 0);
+            if (string.IsNullOrEmpty(token) || now >= expires)
+            {
+                // If all tokens have expired, clean cache
+                CleanAccessTokenCache();
+            }
             return token;
         }
 
-        private void CacheAccessToken(JwtAuthResponse response)
+        private static void CacheAccessToken(JwtAuthResponse response)
         {
-            if (string.IsNullOrEmpty(response?.AccessToken) || response.ExpiresIn == 0)
-                return;
-
-            if (!_jwtTokens.ContainsKey(response.AccessToken))
+            lock (_lock)
             {
-                var now = DateTime.UtcNow.ToTotalSeconds();
-                _jwtTokens.Add(response.AccessToken, now + (ulong)response.ExpiresIn);
+                if (_jwtTokens.ContainsKey(response.AccessToken))
+                    return;
+
+                if (!string.IsNullOrEmpty(response.AccessToken) && response.ExpiresIn > 0)
+                {
+                    var now = DateTime.UtcNow.ToTotalSeconds();
+                    var expireTimestamp = now + response.ExpiresIn;
+                    _jwtTokens.Add(response.AccessToken, expireTimestamp);
+                }
             }
         }
+
+        private static void CleanAccessTokenCache()
+        {
+            var now = DateTime.UtcNow.ToTotalSeconds();
+            lock (_lock)
+            {
+                var keys = _jwtTokens.Keys.ToList();
+                for (var i = 0; i < keys.Count; i++)
+                {
+                    var token = keys[i];
+                    var expires = _jwtTokens[token];
+                    if (now >= expires)
+                    {
+                        _jwtTokens.Remove(token);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region Header Methods
 
         private static Metadata CreateMetadata(string token)
         {
             var headers = new Metadata
             {
                 { AuthorizationHeader, $"Bearer {token}" }
+            };
+            return headers;
+        }
+
+        private static Metadata CreateJwtMetadata()
+        {
+            var headers = new Metadata
+            {
+                { IgnoreJwtValidationHeader, "1" },
             };
             return headers;
         }
