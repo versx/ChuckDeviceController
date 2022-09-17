bb242c246160b28d0c3858308449020a6ab8ad19
versx <versx@ver.sx>
2022-09-17 15:01:15 -0700

    disable change tracking for ControllDbContext

--- src/Libs/ChuckDeviceController.Data/Contexts/ControllerDbContext.cs
+++ src/Libs/ChuckDeviceController.Data/Contexts/ControllerDbContext.cs
@@ -13,7 +13,7 @@
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
             : base(options)
         {
-            //base.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            base.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
         }
 
         public DbSet<Account> Accounts { get; set; }
