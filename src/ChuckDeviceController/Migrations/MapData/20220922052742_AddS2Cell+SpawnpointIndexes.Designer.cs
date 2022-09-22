063671bd55b643f50db6114edeb47c70dec644f5
versx <versx@ver.sx>
2022-09-21 22:37:49 -0700

    add s2cell and spawnpoint indexes migration

--- src/ChuckDeviceController/Migrations/MapData/20220922052742_AddS2Cell+SpawnpointIndexes.Designer.cs
+++ src/ChuckDeviceController/Migrations/MapData/20220922052742_AddS2Cell+SpawnpointIndexes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChuckDeviceController.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChuckDeviceController.Migrations.MapData
 {
     [DbContext(typeof(MapDbContext))]
-    partial class MapDataContextModelSnapshot : ModelSnapshot
+    [Migration("20220922052742_AddS2Cell+SpawnpointIndexes")]
+    partial class AddS2CellSpawnpointIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
