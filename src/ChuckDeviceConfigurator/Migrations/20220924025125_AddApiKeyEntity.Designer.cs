08d864177597c3c2dab0c9f60130b8dc4b953926
versx <versx@ver.sx>
2022-09-23 19:51:42 -0700

    add migration to correct project

--- src/ChuckDeviceConfigurator/Migrations/20220924025125_AddApiKeyEntity.Designer.cs
+++ src/ChuckDeviceConfigurator/Migrations/20220924025125_AddApiKeyEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChuckDeviceController.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChuckDeviceConfigurator.Migrations
 {
     [DbContext(typeof(ControllerDbContext))]
-    partial class DeviceControllerContextModelSnapshot : ModelSnapshot
+    [Migration("20220924025125_AddApiKeyEntity")]
+    partial class AddApiKeyEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
