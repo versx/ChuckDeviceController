07ca354e5f6176f711c0f69516025e07bd769844
versx <versx@ver.sx>
2022-09-24 00:53:46 -0700

    add api key management pages

--- src/ChuckDeviceConfigurator/Controllers/ApiKeyController.cs
+++ src/ChuckDeviceConfigurator/Controllers/ApiKeyController.cs
@@ -41,9 +41,29 @@
         }
 
         // GET: ApiKeyController/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(uint id)
         {
-            return View();
+            var apiKey = await _context.ApiKeys.FindAsync(id);
+            if (apiKey == null)
+            {
+                // Failed to retrieve API key from database, does it exist?
+                ModelState.AddModelError("ApiKey", $"API key does not exist with id '{id}'.");
+                return View();
+            }
+            var scope = ((PluginApiKeyScope[])Enum.GetValues(typeof(PluginApiKeyScope)))
+                .Where(scope => scope != PluginApiKeyScope.None)
+                .Select(scope => new ApiKeyScopeViewModel { Scope = scope, Selected = apiKey.Scope?.Contains(scope) ?? false })
+                .ToList();
+            var model = new ManageApiKeyViewModel
+            {
+                Id = apiKey.Id,
+                Name = apiKey.Name,
+                Key = apiKey.Key,
+                Expiration = apiKey.ExpirationTimestamp.FromSeconds(),
+                Scope = scope,
+                IsEnabled = apiKey.IsEnabled,
+            };
+            return View(model);
         }
 
         // GET: ApiKeyController/Create
@@ -53,7 +73,7 @@
                 .Where(scope => scope != PluginApiKeyScope.None)
                 .Select(scope => new ApiKeyScopeViewModel { Scope = scope, Selected = false })
                 .ToList();
-            var model = new CreateApiKeyViewModel
+            var model = new ManageApiKeyViewModel
             {
                 Scope = scopes,
             };
@@ -63,10 +83,16 @@
         // POST: ApiKeyController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(CreateApiKeyViewModel model)
+        public async Task<ActionResult> Create(ManageApiKeyViewModel model)
         {
             try
             {
+                if (string.IsNullOrEmpty(model?.Name))
+                {
+                    // TODO: Error
+                    return View();
+                }
+
                 var scope = model.Scope
                     .Where(scope => scope.Selected)
                     .Select(scope => scope.Scope)
@@ -96,43 +122,120 @@
         }
 
         // GET: ApiKeyController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(uint id)
         {
-            return View();
+            var apiKey = await _context.ApiKeys.FindAsync(id);
+            if (apiKey == null)
+            {
+                // Failed to retrieve API key from database, does it exist?
+                ModelState.AddModelError("ApiKey", $"API key does not exist with id '{id}'.");
+                return View();
+            }
+            var scope = ((PluginApiKeyScope[])Enum.GetValues(typeof(PluginApiKeyScope)))
+                .Where(scope => scope != PluginApiKeyScope.None)
+                .Select(scope => new ApiKeyScopeViewModel { Scope = scope, Selected = apiKey.Scope?.Contains(scope) ?? false })
+                .ToList();
+            var model = new ManageApiKeyViewModel
+            {
+                Id = apiKey.Id,
+                Name = apiKey.Name,
+                Key = apiKey.Key,
+                Expiration = apiKey.ExpirationTimestamp.FromSeconds(),
+                Scope = scope,
+                IsEnabled = apiKey.IsEnabled,
+            };
+            return View(model);
         }
 
         // POST: ApiKeyController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(uint id, ManageApiKeyViewModel model)
         {
             try
             {
+                var apiKey = await _context.ApiKeys.FindAsync(id);
+                if (apiKey == null)
+                {
+                    // Failed to retrieve API key from database, does it exist?
+                    ModelState.AddModelError("ApiKey", $"API key does not exist with id '{id}'.");
+                    return View();
+                }
+                var scope = model.Scope
+                    .Where(scope => scope.Selected)
+                    .Select(scope => scope.Scope)
+                    .ToList();
+                var seconds = model.Expiration.Ticks == 0
+                    ? 0
+                    : model.Expiration.ToTotalSeconds();
+
+                apiKey.Name = model?.Name ?? string.Empty;
+                apiKey.ExpirationTimestamp = seconds;
+                apiKey.Scope = scope;
+                apiKey.IsEnabled = model?.IsEnabled ?? false;
+
+                _context.ApiKeys.Update(apiKey);
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                ModelState.AddModelError("ApiKey", $"Unknown error occurred while editing API key '{id}'.");
                 return View();
             }
         }
 
         // GET: ApiKeyController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(uint id)
         {
-            return View();
+            var apiKey = await _context.ApiKeys.FindAsync(id);
+            if (apiKey == null)
+            {
+                // Failed to retrieve API key from database, does it exist?
+                ModelState.AddModelError("ApiKey", $"API key does not exist with id '{id}'.");
+                return View();
+            }
+            var scope = ((PluginApiKeyScope[])Enum.GetValues(typeof(PluginApiKeyScope)))
+                .Where(scope => scope != PluginApiKeyScope.None)
+                .Select(scope => new ApiKeyScopeViewModel { Scope = scope, Selected = apiKey.Scope?.Contains(scope) ?? false })
+                .ToList();
+            var model = new ManageApiKeyViewModel
+            {
+                Id = apiKey.Id,
+                Name = apiKey.Name,
+                Key = apiKey.Key,
+                Expiration = apiKey.ExpirationTimestamp.FromSeconds(),
+                Scope = scope,
+                IsEnabled = apiKey.IsEnabled,
+            };
+            return View(model);
         }
 
         // POST: ApiKeyController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(uint id, ManageApiKeyViewModel model)
         {
             try
             {
+                var apiKey = await _context.ApiKeys.FindAsync(id);
+                if (apiKey == null)
+                {
+                    // Failed to retrieve API key from database, does it exist?
+                    ModelState.AddModelError("ApiKey", $"API key does not exist with id '{id}'.");
+                    return View();
+                }
+
+                // Delete device from database
+                _context.ApiKeys.Remove(apiKey);
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                ModelState.AddModelError("ApiKey", $"Unknown error occurred while deleting API key '{id}'.");
                 return View();
             }
         }
--- src/ChuckDeviceConfigurator/ViewModels/ManageApiKeyViewModel.cs
+++ src/ChuckDeviceConfigurator/ViewModels/ManageApiKeyViewModel.cs
@@ -2,12 +2,16 @@
 {
     using System.ComponentModel;
 
-    using ChuckDeviceController.Common.Data.Contracts;
-
-    public class CreateApiKeyViewModel
+    public class ManageApiKeyViewModel
     {
+        [DisplayName("ID")]
+        public uint Id { get; set; }
+
         [DisplayName("Name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
+
+        [DisplayName("API Key")]
+        public string? Key { get; set; }
 
         [DisplayName("Expiration Date")]
         public DateTime Expiration { get; set; }
@@ -17,12 +21,5 @@
 
         [DisplayName("Enabled")]
         public bool IsEnabled { get; set; }
-    }
-
-    public class ApiKeyScopeViewModel
-    {
-        public PluginApiKeyScope Scope { get; set; }
-
-        public bool Selected { get; set; }
     }
 }
