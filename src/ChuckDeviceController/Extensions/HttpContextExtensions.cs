3d686203a62c4edcba19fbf9eaec8679f157159d
versx <versx@ver.sx>
2022-11-12 17:28:00 -0800

    add TryCatch to MAD data converter middleware

--- src/ChuckDeviceController/Extensions/HttpContextExtensions.cs
+++ src/ChuckDeviceController/Extensions/HttpContextExtensions.cs
@@ -16,31 +16,37 @@
 
         public static async Task ConvertPayloadDataAsync(this HttpContext context)
         {
-            // TODO: Add TryCatch
-            // At this point this should be a MAD device sending the request,
-            // retrieve the account username from the Origin HTTP header.
-            var uuid = context.Request.Headers[OriginHeader];
-
-            // Read the request's post body as a string.
-            var data = await context.Request.ReadBodyAsStringAsync();
-            if (string.IsNullOrEmpty(data))
-                return;
-
-            var contents = data.FromJson<List<ProtoData>>();
-            var payload = new ProtoPayload
+            try
             {
-                Contents = contents,
-                Username = DefaultMadUsername,
-                Uuid = uuid,
-            };
+                // At this point this should be a MAD device sending the request,
+                // retrieve the account username from the Origin HTTP header.
+                var uuid = context.Request.Headers[OriginHeader];
 
-            // Replace request stream to downstream handlers
-            var json = payload.ToJson();
-            //var requestData = Encoding.UTF8.GetBytes(json);
-            //context.Request.Body = new MemoryStream(requestData);
-            var content = new StringContent(json, Encoding.UTF8, JsonContentType);
-            context.Request.Body = await content.ReadAsStreamAsync();
-            context.Request.ContentLength = context.Request.Body.Length;
+                // Read the request's post body as a string.
+                var data = await context.Request.ReadBodyAsStringAsync();
+                if (string.IsNullOrEmpty(data))
+                    return;
+
+                var contents = data.FromJson<List<ProtoData>>();
+                var payload = new ProtoPayload
+                {
+                    Contents = contents,
+                    Username = DefaultMadUsername,
+                    Uuid = uuid,
+                };
+
+                // Replace request stream to downstream handlers
+                var json = payload.ToJson();
+                //var requestData = Encoding.UTF8.GetBytes(json);
+                //context.Request.Body = new MemoryStream(requestData);
+                var content = new StringContent(json, Encoding.UTF8, JsonContentType);
+                context.Request.Body = await content.ReadAsStreamAsync();
+                context.Request.ContentLength = context.Request.Body.Length;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ConvertPayloadDataAsync: {ex}");
+            }
         }
 
         public static bool IsPostRequest(this HttpContext context)
