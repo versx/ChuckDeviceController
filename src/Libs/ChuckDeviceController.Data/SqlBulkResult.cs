0e86d8dd54ae9adb9c4a45fdc9d8a73fe1bf10b4
versx <versx@ver.sx>
2022-11-18 17:22:18 -0800

    move SqlQuery and SqlBulk classes

---
+++ src/Libs/ChuckDeviceController.Data/SqlBulkResult.cs
@@ -0,0 +1,21 @@
+﻿namespace ChuckDeviceController.Data
+{
+    public sealed class SqlBulkResult
+    {
+        public bool Success { get; }
+
+        public int BatchCount { get; }
+
+        public int RowsAffected { get; }
+
+        public int ExpectedCount { get; }
+
+        public SqlBulkResult(bool success, int batchCount, int rowsAffected, int expectedCount)
+        {
+            Success = success;
+            BatchCount = batchCount;
+            RowsAffected = rowsAffected;
+            ExpectedCount = expectedCount;
+        }
+    }
+}
