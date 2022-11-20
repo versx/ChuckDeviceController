5aa412d5f4b466fdf3693736dfee8283d714f418
versx <versx@ver.sx>
2022-11-19 17:08:03 -0800

    move grpc method from data consumer to data processor class

--- src/ChuckDeviceController/Services/DataConsumerService.cs
+++ src/ChuckDeviceController/Services/DataConsumerService.cs
@@ -1,7 +1,7 @@
 ﻿namespace ChuckDeviceController.Services
 {
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
-    using System.Data;
     using System.Diagnostics;
 
     using Microsoft.Extensions.Options;
@@ -9,45 +9,59 @@
     using ChuckDeviceController.Configuration;
     using ChuckDeviceController.Data;
     using ChuckDeviceController.Data.Entities;
-    using ChuckDeviceController.Protos;
     using ChuckDeviceController.Services.Rpc;
 
     public class DataConsumerService : IDataConsumerService
     {
         #region Constants
 
-        private const uint DataConsumerIntervalS = 3; //10;
-        private const int DefaultMaxBatchSize = 1000;
+        private const int MaxBatchSize = 1000;
+        private const int MaxCapacity = 1024 * 1024;
+        private static readonly int ConcurrencyLevel = Environment.ProcessorCount * 4;
 
         #endregion
 
         #region Variables
 
-        private static readonly ConcurrentDictionaryQueue<SqlQueryType, List<BaseEntity>> _queue = new();
-        private static readonly System.Timers.Timer _timer = new();
-
+        private readonly ConcurrentDictionary<SqlQueryType, ConcurrentBag<BaseEntity>> _queue = new(ConcurrencyLevel, MaxCapacity);
+        private readonly System.Timers.Timer _timer = new();
         private readonly ILogger<IDataConsumerService> _logger;
+        private readonly IWebHostEnvironment _env;
         private readonly IGrpcClientService _grpcClientService;
-        private readonly SqlBulk _bulk;
+        private readonly SqlBulk _bulk = new();
         private readonly Dictionary<SqlQueryType, (string, string)> _sqlCache = new();
         private readonly IEnumerable<SqlQueryType> _fortDetailTypes = new[]
         {
             SqlQueryType.PokestopDetailsOnMergeUpdate,
             SqlQueryType.GymDetailsOnMergeUpdate,
         };
+        private readonly IEnumerable<string> _fortDetailColumns = new[]
+        {
+            "Id",
+            "Name",
+            "Url",
+        };
 
         #endregion
 
+        #region Properties
+
         public DataConsumerOptionsConfig Options { get; }
+
+        public bool ShowBenchmarkTimes => _env?.IsDevelopment() ?? false;
+
+        #endregion
 
         #region Constructor
 
         public DataConsumerService(
             ILogger<IDataConsumerService> logger,
+            IWebHostEnvironment env,
             IGrpcClientService grpcClientService,
             IOptions<DataConsumerOptionsConfig> options)
         {
             _logger = logger;
+            _env = env;
             _grpcClientService = grpcClientService;
 
             Options = options.Value;
@@ -55,8 +69,6 @@
             _timer.Interval = Options.IntervalS * 1000;
             _timer.Elapsed += async (sender, e) => await ConsumeDataAsync(new());
             _timer.Start();
-
-            _bulk = new SqlBulk();
         }
 
         #endregion
@@ -80,33 +92,6 @@
             await Task.CompletedTask;
         }
 
-        public async Task AddEntitiesAsync(SqlQueryType query, IEnumerable<BaseEntity> entities)
-        {
-            try
-            {
-                if (_queue.ContainsKey(query))
-                {
-                    // TODO: Look into index outside of bounds exception. (list too big?)
-                    _queue[query].AddRange(entities);
-                }
-                else
-                {
-                    var list = entities.ToList();
-                    if (!_queue.TryAdd(query, list))
-                    {
-                        // Key already exists, add entity to queue
-                        _queue[query].AddRange(entities);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error: {ex}");
-            }
-
-            await Task.CompletedTask;
-        }
-
         #endregion
 
         #region Private Methods
@@ -114,9 +99,7 @@
         private async Task ConsumeDataAsync(CancellationToken stoppingToken)
         {
             if (!_queue.Any())
-            {
                 return;
-            }
 
             try
             {
@@ -158,35 +141,24 @@
                     }
 
                     var includedProperties = _fortDetailTypes.Contains(sqlType)
-                        ? new[] { "id", "name", "url" }
+                        ? _fortDetailColumns
                         : null;
                     var result = await _bulk.InsertBulkRawAsync(
                         sqlQuery,
                         sqlValues,
                         entities,
-                        batchSize: Options?.MaximumBatchSize ?? DefaultMaxBatchSize,
+                        batchSize: Options?.MaximumBatchSize ?? MaxBatchSize,
                         includedProperties,
                         null,
                         stoppingToken
                     );
                     if (!result.Success)
                     {
-                        _logger.LogError($"Failed to insert {entities.Count:N0} entities");
-                        continue;
+                        _logger.LogError($"Failed to insert {entityCount:N0} entities");
+                        //continue;
                     }
                     results.Add(result);
                 }
-
-                // TODO: Move to DataProcessorService to be completely stateless
-                //var pokemon = entitiesToUpsert
-                //    .SelectMany(x => x.Value)
-                //    .Where(x => x.GetType() == typeof(Pokemon))
-                //    .Select(x => (Pokemon)x)
-                //    .ToList();
-                //if (pokemon.Any())
-                //{
-                //    await SendPokemonAsync(pokemon);
-                //}
 
                 sw.Stop();
 
@@ -195,13 +167,13 @@
                 var batchCount = results.Sum(x => x.BatchCount);
                 var expectedCount = results.Sum(x => x.ExpectedCount);
 
-                PrintBenchmarkResults(
-                    DataLogLevel.Summary,
+                PrintBenchmarkResults(DataLogLevel.Summary,
                     new BenchmarkResults(
                         rowsAffected, expectedCount, batchCount,
                         "total entities", totalSeconds, sw)
                 );
 
+                ProtoDataStatistics.Instance.TotalEntitiesUpserted += (uint)rowsAffected;
                 ProtoDataStatistics.Instance.AddTimeEntry(new(Convert.ToUInt64(entityCount), totalSeconds));
             }
             catch (Exception ex)
@@ -210,65 +182,17 @@
             }
         }
 
-        private async Task SendPokemonAsync(List<Pokemon> pokemon)
-        {
-            var newPokemon = pokemon
-                .Where(pkmn => pkmn.IsNewPokemon)
-                .ToList();
-            var newPokemonWithIV = pokemon
-                .Where(pkmn => pkmn.IsNewPokemonWithIV)
-                .ToList();
-
-            if (newPokemon.Any())
-            {
-                // Fire off gRPC request on a separate thread
-                await Task.Run(() =>
-                {
-                    new Thread(async () =>
-                    {
-                        // Send got Pokemon proto message
-                        await _grpcClientService.SendRpcPayloadAsync(
-                                newPokemon,
-                                PayloadType.PokemonList,
-                                hasIV: false
-                            );
-                    })
-                    { IsBackground = true }.Start();
-                });
-            }
-
-            if (newPokemonWithIV.Any())
-            {
-                // Fire off gRPC request on a separate thread
-                await Task.Run(() =>
-                {
-                    new Thread(async () =>
-                    {
-                        // Send got Pokemon IV proto message
-                        await _grpcClientService.SendRpcPayloadAsync(
-                            newPokemonWithIV,
-                            PayloadType.PokemonList,
-                            hasIV: true
-                        );
-                    })
-                    { IsBackground = true }.Start();
-                });
-            }
-
-            await Task.CompletedTask;
-        }
-
         private void PrintBenchmarkResults(DataLogLevel logLevel, BenchmarkResults results)
         {
-            //var time = string.Empty;
-            //if (ShowBenchmarkTimes)
-            //{
-            results.Stopwatch?.Stop();
-            var totalSeconds = Math.Round(results.Stopwatch?.Elapsed.TotalSeconds ?? 0, 5).ToString("F5");
-            var time = results.Stopwatch != null
-                ? $" in {totalSeconds}s"
-                : string.Empty;
-            //}
+            var time = string.Empty;
+            if (ShowBenchmarkTimes)
+            {
+                results.Stopwatch?.Stop();
+                var totalSeconds = Math.Round(results.Stopwatch?.Elapsed.TotalSeconds ?? 0, 5).ToString("F5");
+                time = results.Stopwatch != null
+                    ? $" in {totalSeconds}s"
+                    : string.Empty;
+            }
             _logger.LogInformation($"{nameof(DataConsumerService)} upserted {results.RowsAffected:N0}/{results.EntityCount:N0} {results.Text} between {results.BatchCount:N0} batches{time}");
         }
 
