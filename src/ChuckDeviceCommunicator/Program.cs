5cf8426abf7bba323dc430fb9108a07ff5322450
versx <versx@ver.sx>
2022-12-03 16:06:40 -0800

    register IGrpcClient impl via DI for Communicator

--- src/ChuckDeviceCommunicator/Program.cs
+++ src/ChuckDeviceCommunicator/Program.cs
@@ -2,6 +2,10 @@ using ChuckDeviceCommunicator.Services;
 using ChuckDeviceCommunicator.Services.Rpc;
 using ChuckDeviceController.Authorization.Jwt.Rpc.Interceptors;
 using ChuckDeviceController.Configuration;
+using ChuckDeviceController.Protos;
+
+
+#region Config
 
 var config = Config.LoadConfig(args, Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
 if (config.Providers.Count() == 2)
@@ -10,23 +14,45 @@ if (config.Providers.Count() == 2)
     // failed to load config provider.
     Environment.FailFast($"Failed to find or load configuration file, exiting...");
 }
+var grpcConfig = new GrpcEndpointsConfig();
+config.Bind("Grpc", grpcConfig);
+
+var webhookConfig = new WebhookRelayConfig();
+config.Bind("Relay", webhookConfig);
+
+#endregion
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseConfiguration(config);
 
+#region Services
+
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddSingleton<AuthHeadersInterceptor>();
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddSingleton<IWebhookRelayService, WebhookRelayService>();
-builder.Services.AddSingleton<IGrpcClientService, GrpcClientService>();
-builder.Services.Configure<WebhookRelayConfig>(builder.Configuration.GetSection("Relay"));
-builder.Services.Configure<GrpcEndpointsConfig>(builder.Configuration.GetSection("Grpc"));
+builder.Services.Configure<WebhookRelayConfig>(config.GetSection("Relay"));
+builder.Services.Configure<GrpcEndpointsConfig>(config.GetSection("Grpc"));
 
+builder.Services.AddSingleton<IWebhookRelayService, WebhookRelayService>();
+builder.Services.AddSingleton<IGrpcClient<WebhookEndpoint.WebhookEndpointClient, WebhookEndpointRequest, WebhookEndpointResponse>, GrpcWebhookEndpointsClient>();
+builder.Services.AddSingleton<AuthHeadersInterceptor>();
+
+if (!string.IsNullOrEmpty(grpcConfig.Configurator))
+{
+    builder.Services
+        .AddGrpcClient<WebhookEndpoint.WebhookEndpointClient>(options => options.Address = new Uri(grpcConfig.Configurator))
+        .AddInterceptor<AuthHeadersInterceptor>();
+}
+
+#endregion
+
+#region App Builder
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<WebhookPayloadReceiverService>();
 app.Run();
+
+#endregion
