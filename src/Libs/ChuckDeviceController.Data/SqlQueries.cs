b5cb08e4850c85491284edd669658e37918b1414
versx <versx@ver.sx>
2022-12-11 00:23:04 -0800

    fix typo with accounts query

--- src/Libs/ChuckDeviceController.Data/SqlQueries.cs
+++ src/Libs/ChuckDeviceController.Data/SqlQueries.cs
@@ -716,7 +716,7 @@ ON DUPLICATE KEY UPDATE
 (
     {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9},
     {10}, {11}, {12}, {13}, {14}, {15}, {16}, {17}, {18}
-}
+)
 ";
 
         public const string AccountLevelUpdate = @"
