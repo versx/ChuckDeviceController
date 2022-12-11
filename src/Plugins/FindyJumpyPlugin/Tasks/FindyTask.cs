12eeaeb12d88381325b4391022e720735ab0b7fd
versx <versx@ver.sx>
2022-12-10 21:44:09 -0800

    fix json deserialization for FindyJumpyPlugin job controller tasks

--- src/Plugins/FindyJumpyPlugin/Tasks/FindyTask.cs
+++ src/Plugins/FindyJumpyPlugin/Tasks/FindyTask.cs
@@ -1,17 +1,24 @@
 ﻿namespace FindyJumpyPlugin.Tasks
 {
+    using System.Text.Json.Serialization;
+
     using ChuckDeviceController.Common.Tasks;
 
     public class FindyTask : ITask
     {
+        [JsonPropertyName("action")]
         public virtual string Action { get; set; } = null!;
 
+        [JsonPropertyName("lat")]
         public virtual double Latitude { get; set; }
 
+        [JsonPropertyName("lon")]
         public virtual double Longitude { get; set; }
 
+        [JsonPropertyName("min_level")]
         public virtual ushort MinimumLevel { get; set; }
 
+        [JsonPropertyName("max_level")]
         public virtual ushort MaximumLevel { get; set; }
 
         public override string ToString()
