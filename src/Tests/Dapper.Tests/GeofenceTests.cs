b5a64dd2b013781d78afd5c9b4038b241fe7a383
versx <versx@ver.sx>
2022-12-27 00:28:20 -0800

    add geofence and instance unit tests

---
+++ src/Tests/Dapper.Tests/GeofenceTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Dapper.Tests;
+
+using ChuckDeviceController.Data.Common;
+using ChuckDeviceController.Data.Entities;
+using ChuckDeviceController.Data.Factories;
+using ChuckDeviceController.Extensions.Json;
+using ChuckDeviceController.Extensions.Json.Converters;
+
+internal class GeofenceTests
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestGeofenceData()
+    {
+        var json = """
+{
+  "name": "TestGeofence",
+  "type": "geofence",
+  "data":
+  {
+    "area": [
+      {
+        "lat": 34.01,
+        "lon": -117.01
+      },
+      {
+        "lat": 34.02,
+        "lon": -117.02
+      },
+      {
+        "lat": 34.03,
+        "lon": -117.03
+      },
+      {
+        "lat": 34.04,
+        "lon": -117.04
+      }
+    ]
+  }
+}
+""";
+
+        var geofence = json.FromJson<Geofence>(new[] { new ObjectDataConverter<GeofenceData>() });//DbContextFactory.JsonDictionaryConverters);
+        var area = geofence!.Data!.Area;
+        var area2 = geofence!.Data["area"];
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(area == area2, Is.True);
+            Assert.That(geofence, Is.Not.Null);
+        });
+    }
+}
---
+++ src/Tests/Dapper.Tests/InstanceTests.cs
@@ -0,0 +1,79 @@
+﻿namespace Dapper.Tests;
+
+using ChuckDeviceController.Data.Common;
+using ChuckDeviceController.Data.Entities;
+using ChuckDeviceController.Data.Factories;
+using ChuckDeviceController.Extensions.Json;
+using ChuckDeviceController.Extensions.Json.Converters;
+
+internal class InstanceTests
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestInstanceData()
+    {
+        var json = """
+{
+  "name": "TestInstance",
+  "type": "custom",
+  "min_level": 30,
+  "max_level": 39,
+  "geofences": ["TestGeofence"],
+  "data":
+  {
+    "circle_route_type": "Smart",
+    "optimize_dynamic_route": false,
+    "timezone": null,
+    "enable_dst": null,
+    "spin_limit": null,
+    "ignore_s2_cell_bootstrap": null,
+    "use_warning_accounts": null,
+    "quest_mode": "Normal",
+    "max_spin_attempts": 0,
+    "logout_delay": 0,
+    "iv_queue_limit": null,
+    "iv_list": null,
+    "enable_lure_encounters": null,
+    "fast_bootstrap_mode": null,
+    "circle_size": null,
+    "optimize_bootstrap_route": false,
+    "bootstrap_complete_instance_name": null,
+    "optimize_spawnpoints_route": false,
+    "only_unknown_spawnpoints": false,
+    "leveling_radius": 0,
+    "store_leveling_data": false,
+    "leveling_start_coordinate": null,
+    "account_group": null,
+    "is_event": null,
+    "custom_instance_type": "test_controller"
+  }
+}
+""";
+
+        var instance = json.FromJson<Instance>(new[] { new ObjectDataConverter<InstanceData>() });//DbContextFactory.JsonDictionaryConverters);
+        var circleRouteType = instance!.Data!.CircleRouteType;
+        var circleRouteType2 = instance!.Data["circle_route_type"];//.Get<CircleInstanceRouteType>("circle_route_type");
+        //var test = instance!.Get<string>("custom_instance_type", null);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(circleRouteType.ToString() == circleRouteType2?.ToString(), Is.True);
+            Assert.That(instance, Is.Not.Null);
+        });
+    }
+}
+
+public static class DictionaryExtensions
+{
+    public static T? Get<T>(this Dictionary<string, object> dictionary, string key, T? defaultValue = default)
+    {
+        return dictionary.TryGetValue(key, out var value)
+            ? (T?)value
+            : defaultValue;
+    }
+}
