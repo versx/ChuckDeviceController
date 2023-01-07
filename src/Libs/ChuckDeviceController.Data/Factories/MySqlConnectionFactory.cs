93c43dee23ba3117539258f1ee4ef20fcf07f5ca
versx <versx@ver.sx>
2023-01-06 23:41:35 -0800

    add MySqlConnectionFactory

---
+++ src/Libs/ChuckDeviceController.Data/Factories/MySqlConnectionFactory.cs
@@ -0,0 +1,52 @@
+﻿namespace ChuckDeviceController.Data.Factories;
+
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+
+public interface IMySqlConnectionFactory
+{
+    string ConnectionString { get; }
+
+    MySqlConnection CreateConnection(bool open = true);
+
+    Task<MySqlConnection> CreateConnectionAsync(bool open = true, CancellationToken stoppingToken = default);
+}
+
+public class MySqlConnectionFactory : IMySqlConnectionFactory
+{
+    //private readonly IConfiguration _configuration;
+    //private readonly string? _connectionString;
+
+    public string ConnectionString { get; }
+
+    public MySqlConnectionFactory(string connectionString)
+    {
+        ConnectionString = connectionString;
+    }
+
+    public MySqlConnectionFactory(IConfiguration configuration)
+    {
+        //_configuration = configuration;
+        ConnectionString = configuration.GetConnectionString("DefaultConnection")!;
+    }
+
+    public MySqlConnection CreateConnection(bool open = true)
+    {
+        var connection = new MySqlConnection(ConnectionString);
+        if (open)
+        {
+            connection.Open();
+        }
+        return connection;
+    }
+
+    public async Task<MySqlConnection> CreateConnectionAsync(bool open = true, CancellationToken stoppingToken = default)
+    {
+        var connection = new MySqlConnection(ConnectionString);
+        if (open)
+        {
+            await connection.OpenAsync(stoppingToken);
+        }
+        return connection;
+    }
+}
