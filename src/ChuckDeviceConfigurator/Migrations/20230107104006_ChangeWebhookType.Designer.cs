00a909e0b8800202b3a84f33288397269925262a
versx <versx@ver.sx>
2023-01-07 16:45:37 -0800

    add migration for WebhookType

--- src/ChuckDeviceConfigurator/Migrations/20230107104006_ChangeWebhookType.Designer.cs
+++ src/ChuckDeviceConfigurator/Migrations/20230107104006_ChangeWebhookType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ChuckDeviceController.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChuckDeviceConfigurator.Migrations
 {
     [DbContext(typeof(ControllerDbContext))]
-    partial class DeviceControllerContextModelSnapshot : ModelSnapshot
+    [Migration("20230107104006_ChangeWebhookType")]
+    partial class ChangeWebhookType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ChuckDeviceConfigurator/Migrations/20230107104006_ChangeWebhookType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChuckDeviceConfigurator.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeWebhookType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "types",
+                table: "webhook",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "types",
+                table: "webhook",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
