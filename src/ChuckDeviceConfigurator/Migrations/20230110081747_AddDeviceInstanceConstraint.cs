56219261e8fca50a305a06d9b35de69fbcb16b7d
versx <versx@ver.sx>
2023-01-10 01:07:17 -0800

    add device/instance constraints migration

---
+++ src/ChuckDeviceConfigurator/Migrations/20230110081747_AddDeviceInstanceConstraint.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChuckDeviceConfigurator.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceInstanceConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_device_instance_instance_name",
+                table: "device",
+                column: "instance_name",
+                principalTable: "instance",
+                principalColumn: "name",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_device_instance_instance_name",
+                table: "device");
+        }
+    }
+}
